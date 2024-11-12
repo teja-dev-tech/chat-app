3d1b06043d54781f1d2cf034f33161ccf1e55d04
teja-dev-tech <320106410037@andhrauniversity.edu.in>
2024-11-12 20:08:34 +0530

    implemented chat app

---
+++ client/src/redux/store.js
@@ -0,0 +1,13 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import userReducer from './userSlice'
+
+
+export const store = configureStore({
+  reducer: {
+        user : userReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+})
